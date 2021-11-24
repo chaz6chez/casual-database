466a90bad2b6f9bcec1da9a68fc19ef60edc9c54
chaz6chez <250220719@qq.com>
2021-11-25 00:15:33 +0800

    1. 增加自动部署配置

---
+++ database.php
@@ -0,0 +1,39 @@
+<?php
+return [
+    'mysql' => [
+        'demo' => [
+            'driver'   => 'mysql',
+            'host'     => E('mysql.host'),
+            'port'     => E('mysql.port'),
+            'dsn'      => '',
+            'username' => E('mysql.username'),
+            'password' => E('mysql.password'),
+            'charset'  => 'utf8mb4',
+            'dbname'   => 'demo',
+            'option'   => [
+                PDO::ATTR_PERSISTENT       => true, # 长连接
+                PDO::ATTR_TIMEOUT          => 2,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ],
+            'prefix'   => E('mysql.prefix'),
+            'error'    => PDO::ERRMODE_EXCEPTION,
+            'slave'    => [
+                'driver'   => 'mysql',
+                'host'     => E('mysql.host'),
+                'port'     => E('mysql.port'),
+                'dsn'      => '',
+                'username' => E('mysql.username'),
+                'password' => E('mysql.password'),
+                'charset'  => 'utf8mb4',
+                'dbname'   => 'demo',
+                'option'   => [
+                    PDO::ATTR_PERSISTENT       => true, # 长连接
+                    PDO::ATTR_TIMEOUT          => 2,
+                    PDO::ATTR_EMULATE_PREPARES => false
+                ],
+                'prefix'   => E('mysql.prefix'),
+                'error'    => PDO::ERRMODE_EXCEPTION,
+            ]
+        ],
+    ]
+];
